388840dbeb148ceb3fbea71a7856a22b7553af1b
ADsglt77 <adrien.segalat@gmail.com>
2026-02-15 17:37:21 +0100

    🔧 - (config) Add check and close icons to icons data

--- frontend/src/data/icons.ts
+++ frontend/src/data/icons.ts
@@ -21,7 +21,6 @@ export const iconMysql = "devicon:mysql";
 export const iconPdf = "iconoir:file-pdf";
 export const iconLink = "iconoir:link";
 export const iconGithubAction = "iconoir:github";
-
 export const iconPlay = "iconoir:play";
 export const iconSend = "iconoir:send-mail";
 export const iconDownload = "iconoir:download";
@@ -30,6 +29,10 @@ export const iconFastArrowDown = "iconoir:fast-arrow-down";
 export const iconSoundOff = "iconoir:sound-off";
 export const iconSoundHigh = "iconoir:sound-high";
 export const iconSettings = "iconoir:settings";
+
+// Material Symbols
+export const iconCheck = "material-symbols:check-rounded";
+export const iconClose = "material-symbols:close-rounded";
 
 // Autres
 export const iconSymfony = "mdi:symfony";
